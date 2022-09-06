3d9bed4b0557e846224b1ad5333fee038b29b592
Sadie Heintzelman <sadie.heintzelman@gmail.com>
2022-09-06 14:55:18 -0400

    finished listed tasks, image still to be replaced, untested tasks still to be added

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,27 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
+            <h1>This is a header. The text is rather large. Neat.</h1>
             <p>Sara Heintzelman</p>
             <p>Hello World</p>
             <img
                 src="https://cdn.discordapp.com/attachments/601604932906516500/1011155477457031188/unknown.png"
                 alt="A Breaking Bad meme with Walter White looking at cute bunnies. This will be replaced with a different image if I remember to do so."
             />
+            <p>List of the four best Pokemon, objectively.</p>
+            <ol>
+                <li>Dunsparce</li>
+                <li>Dragonite</li>
+                <li>Goodra</li>
+                <li>Lugia</li>
+            </ol>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
-            <Button onClick={() => console.log("I am logged")}>Click Me</Button>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
         </div>
     );
 }
