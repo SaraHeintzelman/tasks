bfaffd8176dfe7436f1420c82537dd5e3d7bb4fc
Sadie Heintzelman <sadie.heintzelman@gmail.com>
2022-09-06 14:47:43 -0400

    temporary image and alt text added

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,10 @@ function App(): JSX.Element {
             </header>
             <p>Sara Heintzelman</p>
             <p>Hello World</p>
+            <img
+                src="https://cdn.discordapp.com/attachments/601604932906516500/1011155477457031188/unknown.png"
+                alt="A Breaking Bad meme with Walter White looking at cute bunnies. This will be replaced with a different image if I remember to do so."
+            />
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
