ac14fb473751008bee588b5e48e52de851030969
Sadie Heintzelman <sadie.heintzelman@gmail.com>
2022-09-06 18:14:32 -0400

    attempted to add two-column layout and rectancles, failed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
@@ -13,7 +13,7 @@ function App(): JSX.Element {
             <p>Hello World</p>
             <img
                 src="https://cdn.discordapp.com/attachments/601604932906516500/1011155477457031188/unknown.png"
-                alt="A Breaking Bad meme with Walter White looking at cute bunnies. This will be replaced with a different image if I remember to do so."
+                alt="A Breaking Bad meme with Walter White looking at bunnies. This will be replaced with a different image if I remember to do so."
             />
             <p>List of the four best Pokemon, objectively.</p>
             <ol>
@@ -29,6 +29,20 @@ function App(): JSX.Element {
             <Button onClick={() => console.log("Hello World!")}>
                 Log Hello World
             </Button>
+            <Container>
+                <Row>
+                    <Col>
+                        <div>
+                            <rect width="100" height="50" color="#E73236" />
+                        </div>
+                    </Col>
+                    <Col>
+                        <div>
+                            <rect width="100" height="50" color="#E73236" />
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
