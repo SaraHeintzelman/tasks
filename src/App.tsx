0ff54da86db2dbbca330988f06e3acb4b5f40473
Sadie Heintzelman <sadie.heintzelman@gmail.com>
2022-09-07 09:48:08 -0400

    changed image and alt text, added rectangles and code in css and tsx files, and added rectangles to the two column layout at the bottom of the page. all tests are passing.

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,8 @@ function App(): JSX.Element {
             <p>Sara Heintzelman</p>
             <p>Hello World</p>
             <img
-                src="https://cdn.discordapp.com/attachments/601604932906516500/1011155477457031188/unknown.png"
-                alt="A Breaking Bad meme with Walter White looking at bunnies. This will be replaced with a different image if I remember to do so."
+                src="https://cdn.discordapp.com/attachments/404501943046635521/1017062602221834351/unknown.png"
+                alt="A mottled grey background with a stock art image of a frog on the side, without the white background removed, and the text 'Graphic design is my passion'"
             />
             <p>List of the four best Pokemon, objectively.</p>
             <ol>
@@ -33,12 +33,12 @@ function App(): JSX.Element {
                 <Row>
                     <Col>
                         <div>
-                            <rect width="100" height="50" color="#E73236" />
+                            <div className="rectangle"></div>
                         </div>
                     </Col>
                     <Col>
                         <div>
-                            <rect width="100" height="50" color="#E73236" />
+                            <div className="rectangle"></div>
                         </div>
                     </Col>
                 </Row>
