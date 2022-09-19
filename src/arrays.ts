4947311418e248c36610e94632fd1f79d5b3f98c
Sadie Heintzelman <sadie.heintzelman@gmail.com>
2022-09-19 13:15:23 -0400

    finished allRBG

--- src/arrays.ts
+++ src/arrays.ts
@@ -60,7 +60,11 @@ export function countShortWords(words: string[]): number {
  * then return true.
  */
 export function allRGB(colors: string[]): boolean {
-    return false;
+    const everyRGB = colors.every(
+        (color: string): boolean =>
+            color === "red" || color === "blue" || color === "green"
+    );
+    return everyRGB;
 }
 
 /**
