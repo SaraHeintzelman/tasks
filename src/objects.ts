1abc70152f91facdd34ff44f15a0ab600bc23909
Sadie Heintzelman <sadie.heintzelman@gmail.com>
2022-09-21 09:30:08 -0400

    finished makeBlankQuestion

--- src/objects.ts
+++ src/objects.ts
@@ -1,16 +1,29 @@
+import { stringify } from "querystring";
 import { Question, QuestionType } from "./interfaces/question";
 
+// 9 incomplete tests left
 /**
  * Create a new blank question with the given `id`, `name`, and `type. The `body` and
  * `expected` should be empty strings, the `options` should be an empty list, the `points`
  * should default to 1, and `published` should default to false.
+ * ---COMPLETE
  */
 export function makeBlankQuestion(
     id: number,
     name: string,
     type: QuestionType
 ): Question {
-    return {};
+    const blank = {
+        id: id,
+        name: name,
+        type: type,
+        body: "",
+        expected: "",
+        options: [],
+        points: 1,
+        published: false
+    };
+    return blank;
 }
 
 /**
