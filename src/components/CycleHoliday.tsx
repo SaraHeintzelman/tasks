48b3033a66c613edb29e98d44176fc1edf3b86f5
Sadie Heintzelman <sadie.heintzelman@gmail.com>
2022-09-30 20:25:30 -0400

    finished CycleHoliday

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,46 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
+// Christmas, Easter, Halloween, Hanukkah, and Fasnacht Day
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<string>("🍩");
+    function advanceYear(): void {
+        if (holiday === "🍩") {
+            setHoliday("🐇");
+        } else if (holiday === "🐇") {
+            setHoliday("🎃");
+        } else if (holiday === "🎃") {
+            setHoliday("🕎");
+        } else if (holiday === "🕎") {
+            setHoliday("🎅");
+        } else {
+            setHoliday("🍩"); // if it's christmas, go to fasnacht day
+        }
+    }
+    function advanceAlphabet(): void {
+        if (holiday === "🎅") {
+            setHoliday("🐇");
+        } else if (holiday === "🐇") {
+            setHoliday("🍩");
+        } else if (holiday === "🍩") {
+            setHoliday("🎃");
+        } else if (holiday === "🎃") {
+            setHoliday("🕎");
+        } else {
+            setHoliday("🎅"); // if it's hanukkah, go to christmas
+        }
+    }
+    return (
+        <div>
+            <span>Holiday: {holiday}</span>
+            <div>
+                <Button onClick={() => advanceYear()}>Advance by year</Button>
+            </div>
+            <div>
+                <Button onClick={() => advanceAlphabet()}>
+                    Advance by alphabet
+                </Button>
+            </div>
+        </div>
+    );
 }
