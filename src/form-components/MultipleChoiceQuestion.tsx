e2cb56cc520be36858aaa0e0fa0fa2e56dbe8810
Sadie Heintzelman <sadie.heintzelman@gmail.com>
2022-10-05 13:02:15 -0400

    finished MultipleChoiceQuestion, finished form-components

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,24 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [currentChoice, setCurrentChoice] = useState<string>(options[0]);
+    function updateChoice(event: React.ChangeEvent<HTMLSelectElement>) {
+        setCurrentChoice(event.target.value);
+    }
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group>
+                <Form.Label>Select an answer below:</Form.Label>
+                <Form.Select value={currentChoice} onChange={updateChoice}>
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <div>{currentChoice === expectedAnswer ? "✔️" : "❌"}</div>
         </div>
     );
 }
