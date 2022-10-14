6f850a941d151d5e36948dc277c5cd0cae57a9b7
Sadie Heintzelman <sadie.heintzelman@gmail.com>
2022-10-14 13:46:09 -0400

    commented out unneeded components and imports, starting Quizzer

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 //import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
-import { ChangeType } from "./components/ChangeType";
-import { RevealAnswer } from "./components/RevealAnswer";
-import { StartAttempt } from "./components/StartAttempt";
-import { TwoDice } from "./components/TwoDice";
-import { CycleHoliday } from "./components/CycleHoliday";
-import { Counter } from "./components/Counter";
-import { DoubleHalf } from "./bad-components/DoubleHalf";
-import { ColoredBox } from "./bad-components/ColoredBox";
-import { ShoveBox } from "./bad-components/ShoveBox";
-import { ChooseTeam } from "./bad-components/ChooseTeam";
-import { CheckAnswer } from "./form-components/CheckAnswer";
-import { GiveAttempts } from "./form-components/GiveAttempts";
-import { EditMode } from "./form-components/EditMode";
-import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
-import { ChangeColor } from "./form-components/ChangeColor";
+//import { ChangeType } from "./components/ChangeType";
+//import { RevealAnswer } from "./components/RevealAnswer";
+//import { StartAttempt } from "./components/StartAttempt";
+//import { TwoDice } from "./components/TwoDice";
+//import { CycleHoliday } from "./components/CycleHoliday";
+//import { Counter } from "./components/Counter";
+//import { DoubleHalf } from "./bad-components/DoubleHalf";
+//import { ColoredBox } from "./bad-components/ColoredBox";
+//import { ShoveBox } from "./bad-components/ShoveBox";
+//import { ChooseTeam } from "./bad-components/ChooseTeam";
+//import { CheckAnswer } from "./form-components/CheckAnswer";
+//import { GiveAttempts } from "./form-components/GiveAttempts";
+//import { EditMode } from "./form-components/EditMode";
+//import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
+//import { ChangeColor } from "./form-components/ChangeColor";
 import { Quizzer } from "./quizzer/Quizzer";
 
 function App(): JSX.Element {
@@ -25,6 +25,7 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <Quizzer></Quizzer>
+            {/*
             <hr></hr>
             <CheckAnswer expectedAnswer="42"></CheckAnswer>
             <hr></hr>
@@ -58,6 +59,7 @@ function App(): JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
+    */}
         </div>
     );
 }
